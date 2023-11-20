c4b8c3d82858a0d1a9226b9c4baa670ba2f3705a
ulfatun_amanah <ulfaamana@gmail.com>
2023-11-20 22:53:32 +0700

    initialize project

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import Home from "./pages/home";
+
+const Routes = [
+  {
+    path: "",
+    component: Home,
+  },
+  {
+    path: "home",
+    component: Home,
+  },
+]
+
+export default Routes;
